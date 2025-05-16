0ca366f419270cd8635f6e110e13368df7c2eb6e
excelenciaempire <juandiegoriosmesa@gmail.com>
2025-05-16 18:12:44 -0400

    refactor: Remove comments from AuthNavigator

--- app-spediak/Spediak/src/navigation/AuthNavigator.tsx
+++ app-spediak/Spediak/src/navigation/AuthNavigator.tsx
@@ -1,36 +1,30 @@
 import React from 'react';
-// import { NavigationContainer } from '@react-navigation/native'; // Remove this import
-// import { createStackNavigator } from '@react-navigation/stack'; // Remove this line
-import { createNativeStackNavigator } from '@react-navigation/native-stack'; // Keep/Restore this line
+import { createNativeStackNavigator } from '@react-navigation/native-stack';
 import LoginScreen from '../screens/auth/LoginScreen';
 import SignUpScreen from '../screens/auth/SignUpScreen';
 import ForgotPasswordScreen from '../screens/auth/ForgotPasswordScreen';
 import TermsScreen from '../screens/auth/TermsScreen';
-import PrivacyPolicyScreen from '../screens/legal/PrivacyPolicyScreen'; // Import new screen
+import PrivacyPolicyScreen from '../screens/legal/PrivacyPolicyScreen';
 
-// Define the stack parameter list
 export type AuthStackParamList = {
   Login: undefined;
   SignUp: undefined;
   ForgotPassword: undefined;
   TermsAndConditions: undefined;
-  PrivacyPolicy: undefined; // Add new route
+  PrivacyPolicy: undefined;
 };
 
-// const Stack = createStackNavigator<AuthStackParamList>(); // Remove this line
-const Stack = createNativeStackNavigator<AuthStackParamList>(); // Keep/Restore this line
+const Stack = createNativeStackNavigator<AuthStackParamList>();
 
 const AuthNavigator: React.FC = () => {
   return (
-    // No NavigationContainer here
     <Stack.Navigator initialRouteName="Login" screenOptions={{ headerShown: false }}>
       <Stack.Screen name="Login" component={LoginScreen} />
       <Stack.Screen name="SignUp" component={SignUpScreen} />
       <Stack.Screen name="ForgotPassword" component={ForgotPasswordScreen} />
       <Stack.Screen name="TermsAndConditions" component={TermsScreen} />
-      <Stack.Screen name="PrivacyPolicy" component={PrivacyPolicyScreen} /> {/* Add new screen */}
+      <Stack.Screen name="PrivacyPolicy" component={PrivacyPolicyScreen} />
     </Stack.Navigator>
-    // No NavigationContainer here
   );
 };
 
